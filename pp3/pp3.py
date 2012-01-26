5b61bc668e72c0901997f8b2ae942549535ab862
Vince <firebat5@hotmail.com>
2012-01-25 21:46:46 -0500

    Added code for createShape in Assignment 3

--- pp3/pp3.py
+++ pp3/pp3.py
@@ -14,5 +14,42 @@ def printBoard(length, height, contents):
 def getPiece(pieceText):
 	a = pieceText.split(' ')
 	return (a[0],int(a[1]),a[2])
-				 
+
+def createShape(shapeName, shapeLength, boardLength):
+	outShape = []
+	if shapeName == 'L_shape':
+		#Generates the first two elements of the L.
+		outShape.append(1)
+		outShape.append(2)
+		
+		#Generates the long part of the L
+		for i in range(1,shapeLength):
+			outShape.append(boardLength*i+1)
+			
+	elif shapeName == 'T_shape':
+		#Generates first element of T
+		outShape.append(2)
+		
+		for i in range(1,shapeLength+1):
+			outShape.append(boardLength+i)
 	
+	elif shapeName == 'X_shape':
+		outShape.append(2)
+		
+		for i in range(1,shapeLength+1):
+			outShape.append(boardLength+i)
+		
+		outShape.append(boardLength*2+2)
+	
+	elif shapeName == 'U_shape':
+		outShape.append(1)
+		outShape.append(2)
+		for i in range(1,shapeLength):
+			outShape.append(boardLength*i+2)
+		outShape.append(boardLength*(shapeLength-1)+1)
+		outShape.sort()
+	
+	elif shapeName == 'I_shape':
+		outShape = range(1,shapeLength+1)
+			
+	return outShape
