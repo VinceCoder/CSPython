7e77fc4d3c31c0e33166c9121f22d420975f844b
Vince <firebat5@hotmail.com>
2012-01-25 21:55:30 -0500

    Added fuction getAllOrderings to Assignment 3. Added invalid shape message to createShape function

--- pp3/pp3.py
+++ pp3/pp3.py
@@ -1,3 +1,5 @@
+import itertools
+
 def printBoard(length, height, contents):
 	if len(contents) != length*height:
 		print 'Invalid board!'
@@ -27,15 +29,12 @@ def createShape(shapeName, shapeLength, boardLength):
 			outShape.append(boardLength*i+1)
 			
 	elif shapeName == 'T_shape':
-		#Generates first element of T
 		outShape.append(2)
-		
 		for i in range(1,shapeLength+1):
 			outShape.append(boardLength+i)
 	
 	elif shapeName == 'X_shape':
 		outShape.append(2)
-		
 		for i in range(1,shapeLength+1):
 			outShape.append(boardLength+i)
 		
@@ -51,5 +50,15 @@ def createShape(shapeName, shapeLength, boardLength):
 	
 	elif shapeName == 'I_shape':
 		outShape = range(1,shapeLength+1)
+	
+	else:
+		print 'Invalid shape!'
+		return
 			
 	return outShape
+
+def getAllOrderings(inList):
+	outList = []
+	for l in itertools.permutations(inList):
+		outList.append(list(l))
+	return outList
