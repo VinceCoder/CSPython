687d0d6d6a14ea0315fd62bcd0b6a4428becba5f
Vince <firebat5@hotmail.com>
2012-02-15 19:36:18 -0500

    Changed some docstrings and converted tabs to spaces

--- pp4/pp4.py
+++ pp4/pp4.py
@@ -2,64 +2,64 @@ import pp2
 import pp3
 
 def fancyPrintBoard(length, height, contents):
-	"""
-	Identical to the printBoard function in pp3 with the difference
-	that it allows use of strings in contents. This is so we can print
-	the labels of the shapes.
-	"""
-	if len(contents) != length*height:
-		print 'Invalid board!'
-	elif not (type(contents) is list):
-		print 'Invalid board!'
-	else:
-		print '|'+'-'*(2*length+1)+'|'
-		for i in range(0,height):
-			print '| '+' '.join(map(str,contents[length*i:length*i+length]))+' |'
-		print '|'+'-'*(2*length+1)+'|'
+    """
+    Identical to the printBoard function in pp3 with the difference
+    that it allows use of strings in contents. This is so we can print
+    the labels of the shapes.
+    """
+    if len(contents) != length*height:
+        print 'Invalid board!'
+    elif not (type(contents) is list):
+        print 'Invalid board!'
+    else:
+        print '|'+'-'*(2*length+1)+'|'
+        for i in range(0,height):
+            print '| '+' '.join(map(str,contents[length*i:length*i+length]))+' |'
+        print '|'+'-'*(2*length+1)+'|'
 
 
 def readParams(fileName):
-	"""
-	Reads the input text file and parses out the height, length, and pieces
+    """
+    Reads the input text file and parses out the height, length, and pieces
     It expects a file with the first line containing length, second line
     containing height, and the next lines containing a shape in the format
     shape_type shape_length shape_label. eg. T_shape 5 T
-	"""
-	f = open(fileName,'r')
-	paramsList = {}
-	paramsList['boardLength'] = (int(f.readline()))
-	paramsList['boardHeight'] = (int(f.readline()))	
-	paramsList['boardPieces'] = []
-	for line in f:
-		paramsList['boardPieces'].append(line.strip())
-	return paramsList
+    """
+    f = open(fileName,'r')
+    paramsList = {}
+    paramsList['boardLength'] = (int(f.readline()))
+    paramsList['boardHeight'] = (int(f.readline())) 
+    paramsList['boardPieces'] = []
+    for line in f:
+        paramsList['boardPieces'].append(line.strip())
+    return paramsList
 
 def convertPiecesToDict(pieces,boardLength):
-	"""
-	This function takes a list of tuples with shape information
-	as input: eg. [(L_shape,4,L), (T_shape,4,T)] and converts them
+    """
+    This function takes a list of tuples with shape information
+    as input: eg. [(L_shape,4,L), (T_shape,4,T)] and converts them
     them to dictionary eg. : {'L':[1,2,6,10], 'T':[2,6,7,8,9]}
-	"""
-	outDict = {}
-	for piece in pieces:
-		splitUp = pp3.getPiece(piece)
-		outDict[splitUp[2]]=pp3.createShape(splitUp[0],splitUp[1],boardLength)
-	return outDict
+    """
+    outDict = {}
+    for piece in pieces:
+        splitUp = pp3.getPiece(piece)
+        outDict[splitUp[2]]=pp3.createShape(splitUp[0],splitUp[1],boardLength)
+    return outDict
 
 def nextFreeSlot(defaultShape,boardHeight,boardLength,boardContents):
-	"""
+    """
     Given a shape, board sizes, and the board's contents, the function
-    goes through all positions starting from 1 and determines
-    the next free position where the shape would fit.
-	"""
-	for i in range(1,boardHeight*boardLength+1):
-		if boardContents[i-1] != 1:
-			shiftedLoc = pp2.pieceFits(boardLength,boardHeight,boardContents,defaultShape,i)
-			if len(shiftedLoc):
-				return shiftedLoc
-			else:
-				return []
-	return []
+    checks whether a piece can fit in the next free slot. If not it returns
+    a blank list [].
+    """
+    for i in range(1,boardHeight*boardLength+1):
+        if boardContents[i-1] != 1:
+            shiftedLoc = pp2.pieceFits(boardLength,boardHeight,boardContents,defaultShape,i)
+            if len(shiftedLoc):
+                return shiftedLoc
+            else:
+                return []
+    return []
 
 
 def isBoardFilled(boardContents):
@@ -70,48 +70,48 @@ def isBoardFilled(boardContents):
 
 def wasSolutionPreviouslyComputed(currentSolution,previousSolutions):
     """
-    Sometimes solutions can be repeated. This function takes a solution as a list (eg. [T,T,T])
-	along with a list of previous solutions (eg. [[I,I,U],[T,T,T],[X,X,U]]). It checks
-	whether the currentSolution exists in previousSolutions
-	"""
+    This function takes a solution as a list (eg. [T,T,T])
+    along with a list of previous solutions (eg. [[I,I,U],[T,T,T],[X,X,U]]). It checks
+    whether the currentSolution exists in previousSolutions
+    """
     return currentSolution in previousSolutions
 
 def main():
-	params = readParams('load.txt')
-	boardHeight = params['boardHeight']
-	boardLength = params['boardLength']
-	boardMax    = boardHeight*boardLength
-	pieces = convertPiecesToDict(params['boardPieces'],boardLength)
-	numSolutions = 0
-	solutions = []
-	
-	# We need to use sorted keys to keep the output order identical to professor's.
-	for combo in pp3.getAllOrderings(sorted(pieces.keys())):
-	
-		# Start with a fresh board for each combination
-		contents = [0]*boardMax
-		fancyContents = ['']*boardMax
-		
-		# Go through each piece and place in the next available free slot on the board
-		for pieceLabel in combo:
-			shiftedShape = nextFreeSlot(pieces[pieceLabel],boardHeight,boardLength,contents)
-			
-			# If a certain shape doesn't fit, go to the next combo.
-			if not shiftedShape:
-				break;
-			
-			#Place piece on the board by updating contents
-			for i in shiftedShape:
-			    contents[i-1] = 1
-			    fancyContents[i-1] = pieceLabel
-			
-			#Check whether we have a solution
-			if isBoardFilled(contents) and not wasSolutionPreviouslyComputed(fancyContents,solutions):
-			    fancyPrintBoard(boardLength,boardHeight,fancyContents)
-			    solutions.append(fancyContents[:])
-			    numSolutions = numSolutions + 1
-			
-	print 'Number of solutions: %d' % (numSolutions)
+    params = readParams('load.txt')
+    boardHeight = params['boardHeight']
+    boardLength = params['boardLength']
+    boardMax    = boardHeight*boardLength
+    pieces = convertPiecesToDict(params['boardPieces'],boardLength)
+    numSolutions = 0
+    solutions = []
+    
+    # We need to use sorted keys to keep the output order identical to professor's.
+    for combo in pp3.getAllOrderings(sorted(pieces.keys())):
+    
+        # Start with a fresh board for each combination
+        contents = [0]*boardMax
+        fancyContents = ['']*boardMax
+        
+        # Go through each piece and place in the next available free slot on the board
+        for pieceLabel in combo:
+            shiftedShape = nextFreeSlot(pieces[pieceLabel],boardHeight,boardLength,contents)
+            
+            # If a certain shape doesn't fit, go to the next combo.
+            if not shiftedShape:
+                break;
+            
+            #Place piece on the board by updating contents
+            for i in shiftedShape:
+                contents[i-1] = 1
+                fancyContents[i-1] = pieceLabel
+            
+            #Check whether we have a solution
+            if isBoardFilled(contents) and not wasSolutionPreviouslyComputed(fancyContents,solutions):
+                fancyPrintBoard(boardLength,boardHeight,fancyContents)
+                solutions.append(fancyContents[:])
+                numSolutions = numSolutions + 1
+            
+    print 'Number of solutions: %d' % (numSolutions)
 
 if __name__ == '__main__':
     main()
